14e6dc4fc3bf4e4c04b16ec3657149060eb66bd8
chenxiaolu5009 <chenxiaolu5009@gmail.com>
2024-11-19 14:31:33 +1100

    update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -5,10 +5,37 @@ Project 1 - The Number Guessing Game
 """
 
 # Import the random module.
-
+import random
 # Create the start_game function.
-# Write your code inside this function.
 
+def start_game():
+# Write your code inside this function.
+    answer = random.randint(1,10)
+    n = 3                               #the times for guess
+
+    while n > 0:
+        try:
+            guess_number = int(input("Plz guess a number between 1 and 10: "))
+
+            if guess_number < 1 or guess_number > 10:
+                print("Please enter a number betwee 1 to 10")
+                continue
+
+            if guess_number > answer:
+                print("Plz guess a smaller number")
+            elif guess_number < answer:
+                print("Plz guess a bigger number ")
+            else: 
+                print("Bingo!")
+                break
+            n -= 1
+        except ValueError:
+            print("Plz enter a number, between 1 and 10")
+    else:
+        print(f"Unfortunately, you do not have anymore time to guess. The correct number is {answer}")
+
+
+    
 #   When the program starts, we want to:
 #   ------------------------------------
 #   1. Display an intro/welcome message to the player.
